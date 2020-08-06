2527a8d28c5e1a0939b6647b4f9db09a72c8514f
Marc Villanueva <mvillanueva@isbx.com>
2020-08-06 19:56:39 +0800

    init

---
+++ server.js
@@ -0,0 +1,63 @@
+import { createServer, Model, Factory, trait } from "miragejs";
+import { add, parseISO } from "date-fns";
+import faker, { image, name, internet, lorem } from "faker";
+
+faker.seed(123);
+
+let startingDate = parseISO("2020-01-14");
+let server = createServer({
+  timing: 1000,
+  models: {
+    tweet: Model,
+  },
+
+  factories: {
+    tweet: Factory.extend({
+      name() {
+        return name.findName();
+      },
+
+      username() {
+        return internet.userName();
+      },
+
+      text() {
+        return lorem.sentence();
+      },
+
+      avatarUrl() {
+        return image.avatar();
+      },
+
+      date(i) {
+        return add(startingDate, { days: i }).toISOString();
+      },
+
+      fromMarc: trait({
+        name: "Marc Villanueva",
+        username: "mrcvllnv",
+        avatarUrl: "http://twivatar.glitch.me/mrcvllnv",
+      }),
+    }),
+  },
+
+  routes() {
+    this.namespace = "api";
+    this.get("tweets");
+
+    this.passthrough();
+  },
+
+  seeds(server) {
+    server.create("tweet", "fromMarc", { text: "just setting up my twttr" });
+    server.create("tweet", "fromMarc", { text: "Hi" });
+    server.create("tweet", "fromMarc", {
+      text: "I still don't understand useEffect",
+    });
+    server.createList("tweet", 50);
+  },
+});
+
+// setInterval(() => {
+//   server.create("tweet");
+// }, 5000);
